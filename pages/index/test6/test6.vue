5cc3521bc8f057b9f4d8d1a3d96a4968d7d9fecc
mao <1296193245@qq.com>
2023-12-13 17:09:31 +0800

    uniapp-network-request-client

---
+++ pages/index/test6/test6.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<button type="primary" @click="conn">发起websocket连接</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			conn() {
+				const userId = "10000001";
+				uni.connectSocket({
+					url: 'ws://localhost:9091/websocket/' + userId,
+					method: 'GET',
+					success: (resp) => {
+						console.log(resp);
+						uni.showToast({
+							title: '连接成功',
+							icon: 'success'
+						})
+					},
+					fail: (err) => {
+						console.log(err);
+						uni.showToast({
+							title: '连接失败',
+							icon: 'error'
+						})
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
